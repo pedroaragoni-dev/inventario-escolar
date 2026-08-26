8882bd29ee7322750288ef5bc15254b8870e4ad1
Pedro Aragoni <pedroaragoni.06@gmail.com>
2026-08-26 12:56:04 -0300

    feat: inicia sistema de inventario escolar

---
+++ src/main/java/br/com/pedroaragoni/inventario/InventarioEscolarApplication.java
@@ -0,0 +1,13 @@
+package br.com.pedroaragoni.inventario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventarioEscolarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventarioEscolarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/pedroaragoni/inventario/InventarioEscolarApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pedroaragoni.inventario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventarioEscolarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
