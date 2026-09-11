ea545be31aaf702f57e2d1fa4f1a2d7f10e89040
Pedro Aragoni <pedroaragoni.06@gmail.com>
2026-09-11 10:22:44 -0300

    Limita nome da categoria a 100 caracteres

--- src/main/java/br/com/pedroaragoni/inventario/dto/CategoriaCadastroRequest.java
+++ src/main/java/br/com/pedroaragoni/inventario/dto/CategoriaCadastroRequest.java
@@ -1,9 +1,12 @@
 package br.com.pedroaragoni.inventario.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public record CategoriaCadastroRequest(
+
         @NotBlank(message = "O nome da categoria é obrigatório")
+        @Size(max = 100, message = "O nome da categoria deve ter no máximo 100 caracteres")
         String nome
 ) {
 }
